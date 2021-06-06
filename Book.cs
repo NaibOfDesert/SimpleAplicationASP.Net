0beb9c26b9ebca288bef0ac1e007a799bb8c9f2a
NaibOfDesert <tryc.artur@gmail.com>
2021-06-06 21:11:48 +0200

    Book class + migrations.

---
+++ Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleAplicationASP.Net
+{
+    [Table("Books")]
+    public class Book
+    {
+        [Key]
+        public int bookId { get; set; }
+        public string bookTitle { get; set; } 
+        public string bookAuthor { get; set; }
+        public int bookDateRelease { get; set; }
+        public BookCoverType bookCover { get; set; }
+
+        public Book() { }
+
+        public Book (int __bookId, string __bookTitle, string __bookAuthor, int __bookDateRelease, BookCoverType __bookCover)
+        {
+            bookId = __bookId;
+            bookTitle = __bookTitle;
+            bookAuthor = __bookAuthor;
+            bookDateRelease = __bookDateRelease;
+            bookCover = __bookCover;
+        }
+
+
+
+
+    }
+}
